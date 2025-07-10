59775af7ecc7c6af78b06baa9ea1a66fb1ed2fa2
Petter Bjørkås <pbjorkaas@enua.no>
2025-07-10 07:59:23 +0200

    Add index validation

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from helper.search import search_for_location
 
 
 fishing_location = None
-
 while (fishing_location is None):
     user_input = input("Enter a location: ")
-    print(f"Searching for location: {user_input}")
 
+    print(f"Searching for location: {user_input}")
     search_result = search_for_location(user_input)
     if (len(search_result) == 0):
         print("Unable to find location.. Try again.")
@@ -15,11 +14,12 @@ while (fishing_location is None):
     if (len(search_result) > 1):
         print("Found multiple locations. Select one from the list: ")
         for index, res in enumerate(search_result):
-            print(f"{index}. {res}")
+            print(f"{index+1}. {res}")
         selected = None
-        selected_index = input("Enter the number matching your location")
-        # TODO: Valudate index..
-        fishing_location = search_result[int(selected_index)]
+        selected_index = int(input("Enter the number matching your location"))
+        while selected_index < 0 or selected_index > len(search_result):
+            selected_index = int(input(f"Invalid choice. Enter a number between {1}-{len(search_result)}"))
+        fishing_location = search_result[selected_index-1]
     else:
         fishing_location = search_result[0]
 
