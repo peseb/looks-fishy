c9934114732eadb73858e65b4325dd471e4c1867
Petter Bjørkås <pbjorkaas@enua.no>
2025-07-10 08:02:09 +0200

    Cleanup

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ while (fishing_location is None):
     print(f"Searching for location: {user_input}")
     search_result = search_for_location(user_input)
     if (len(search_result) == 0):
-        print("Unable to find location.. Try again.")
+        print("Unable to find location.. Try again")
         continue
 
     if (len(search_result) > 1):
         print("Found multiple locations. Select one from the list: ")
         for index, res in enumerate(search_result):
             print(f"{index+1}. {res}")
-        selected = None
+         
         selected_index = int(input("Enter the number matching your location"))
         while selected_index < 0 or selected_index > len(search_result):
             selected_index = int(input(f"Invalid choice. Enter a number between {1}-{len(search_result)}"))
