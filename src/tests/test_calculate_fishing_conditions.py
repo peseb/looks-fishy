9e975176ac67782953c45b57041020bd7ffd8fc0
Petter Bjørkås <pbjorkaas@enua.no>
2025-07-14 13:38:25 +0200

    Start on test for calculate_conditions

--- src/tests/test_calculate_fishing_conditions.py
+++ src/tests/test_calculate_fishing_conditions.py
@@ -1,4 +1,6 @@
 import json
+from helper.calculate_fishing_conditions import calculate_fishing_conditions
+from helper.is_data_for_date import date_from_str, is_data_for_date
 from weather_forecast.weather_response import WeatherResponse
 
 
@@ -12,3 +14,16 @@ def get_mock_data():
 def test_read_mock_data():
     mock_weather = get_mock_data()
     assert len(mock_weather.properties.timeseries) == 86
+
+
+def test_calculate_conditions():
+    mock_weather = get_mock_data()
+    day = date_from_str("2025-07-14T10:27:27Z")
+    day_info = list(
+        filter(
+            lambda x: is_data_for_date(x.time, day),
+            mock_weather.properties.timeseries,
+        )
+    )
+    result = calculate_fishing_conditions(day_info)
+    assert result != ""
